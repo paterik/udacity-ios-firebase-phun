ce82cc9ef0fe3c352a71ce3fa72b8a5287a074bd
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2017-04-19 06:21:28 +0200

    refactor: change viewController name/namespace add new mainViewontrollerExtension

---
+++ FirebasePhun/MainViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewControllerExtension.swift
+//  FirebasePhun
+//
+//  Created by Patrick Paechnatz on 19.04.17.
+//  Copyright © 2017 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
